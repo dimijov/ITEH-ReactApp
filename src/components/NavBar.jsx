1b98e5cabb5b6e6668c5f1ab86c6293c31548ca2
jyxop <2000dimitrije@gmail.com>
2023-03-20 23:45:43 +0100

    Dodat Component props i korpa u NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import navLogo from "../img/logo.png";
+import { FiShoppingCart } from "react-icons/fi";
 
-function NavBar() {
+function NavBar({ cartNum }) {
      return (
           <>
                <div className="navBar">
                     <img className="logoNavBar" src={navLogo} alt="logoNav" />
                     <a href="/">Shop Now</a>
                     <a href="/">Cart</a>
+                    <div className="cart">
+                         {" "}
+                         <FiShoppingCart />
+                         &nbsp;
+                         {cartNum}
+                    </div>
                </div>
           </>
      );
--- src/components/OneItem.jsx
+++ src/components/OneItem.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { GoPlus } from "react-icons/go";
 
-function OneItem() {
+function OneItem({ product, addItem }) {
      return (
           <div className="card">
-               <img src="https:/picsum.photos/350" alt="item" />
-               <div className="cardNamePrice">
-                    <h3>Item name</h3>
-                    <h3>1.000 RSD</h3>
-               </div>
-               <hr className="hr" />
-               <div className="cardAdd">
-                    <h3>Quick add</h3>
-                    <h3>
-                         <GoPlus />
-                    </h3>
+               <img src={product.image} alt="ItemPhoto" />
+
+               <div className="infoWrapper">
+                    <div className="cardNamePrice">
+                         <h3>{product.name}</h3>
+                         <h3>{product.price}</h3>
+                    </div>
+
+                    <hr className="hr" />
+
+                    <div className="cardAdd">
+                         <h3>Quick add</h3>
+                         <button onClick={() => addItem(product.name)}>
+                              <GoPlus />
+                         </button>
+                    </div>
                </div>
           </div>
      );
